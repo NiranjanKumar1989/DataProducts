e117d42c1d0e7e5feeb6565543b90c4a3121cbae
NiranjanKumar1989 <niranjan.srec@gmail.com>
2014-12-21 23:52:10 +0530

    Modifications to the Shiny Application

--- ui.R
+++ ui.R
@@ -1,10 +1,10 @@
 library(shiny)
 
 shinyUI(
-  navbarPage("Just a Shiny Application",
+  navbarPage("Data Products Assignment",
              tabPanel("Analysis",
                       fluidPage(
-                        titlePanel("The relationship between variables and miles per gallon (MPG)"),
+                        titlePanel("Relationship Between MPG and the other Variables"),
                         sidebarLayout(
                           sidebarPanel(
                             selectInput("variable", "Variable:",
@@ -37,7 +37,14 @@ shinyUI(
                         )
                       )
              ),
-             tabPanel("Source Data",
+             tabPanel("Application Documentation",
+                      h3("Application Overview"),
+                      p("The application is used for understanding the relationship between miles per gallon (MPG) and the other variables in the dataset. The reslationship can be visualised using a box plot as well as a regression model. There is a selctor using which the user can select the variable that needs to be used for the comaprison. Also the user has the flexibility to show the outliers or to hide them in the analysis. The outliers checkbos is applicable only to the box plot and all outliers are always included in the regression model. The user can toggle between the box plot and the regression model by clicking on the appropriate tabs. Also there are separate tabs available in this application describing in detail about the source data and the application itself."),
+                      h3("Link to the Github Reposotory"),
+                      p("The following link takes you to the Github repository where the code for this shiny application is available.\n"),
+                      a("https://github.com/NiranjanKumar1989/DataProducts")
+             ),
+             tabPanel("Understanding the Source Data",
                       h2("Motor Trend Car Road Tests"),
                       hr(),
                       h3("Description"),
@@ -47,26 +54,21 @@ shinyUI(
                       h3("Format"),
                       p("A data frame with 32 observations on 11 variables."),
                       
-                      p("  [, 1]   mpg         Miles/(US) gallon"),
-                      p("  [, 2]   cyl	 Number of cylinders"),
-                      p("  [, 3]	 disp	 Displacement (cu.in.)"),
-                      p("  [, 4]	 hp	 Gross horsepower"),
-                      p("  [, 5]	 drat	 Rear axle ratio"),
-                      p("  [, 6]	 wt	 Weight (lb/1000)"),
-                      p("  [, 7]	 qsec	 1/4 mile time"),
-                      p("  [, 8]	 vs	 V/S"),
-                      p("  [, 9]	 am	 Transmission (0 = automatic, 1 = manual)"),
-                      p("  [,10]	 gear	 Number of forward gears"),
-                      p("  [,11]	 carb	 Number of carburetors"),
+                      p("  [, 1]   mpg: Miles/(US) gallon"),
+                      p("  [, 2]   cyl: Number of cylinders"),
+                      p("  [, 3]	 disp: Displacement (cu.in.)"),
+                      p("  [, 4]	 hp: Gross horsepower"),
+                      p("  [, 5]	 drat: Rear axle ratio"),
+                      p("  [, 6]	 wt: Weight (lb/1000)"),
+                      p("  [, 7]	 qsec: 1/4 mile time"),
+                      p("  [, 8]	 vs: V/S"),
+                      p("  [, 9]	 Transmission: (0 = automatic, 1 = manual)"),
+                      p("  [,10]	 gear: Number of forward gears"),
+                      p("  [,11]	 carb: Number of carburetors"),
                       
                       h3("Source"),
                       
                       p("Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391-411.")
-             ),
-             tabPanel("Github repository",
-                      a("https://github.com/ludovicbenistant?tab=repositories"),
-                      hr(),
-                      p("I hope you like the Shiny App. The name of the repositorie is DataProducts")
              )
   )
 )
